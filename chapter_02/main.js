1d48927bb3f9a390d6e51b931a7231c175e6cd10
suzuki84g <suzuki.84g@gmail.com>
2020-06-02 15:11:05 +0900

    進捗p47

--- chapter_02/main.js
+++ chapter_02/main.js
@@ -6,3 +6,23 @@ var app = new Vue({
         message: 'Hello Vue!'
     }
 });
+
+// 「動くモノ」クラスの定義
+var Movable = function (x, y) {
+    this.pos = {
+        x: x,
+        y: y,
+    };
+    this.move = function (x, y) {
+        this.pos.x += x;
+        this.pos.y += y;
+    };
+};
+
+// ボールのインスタンスを生成する
+var ball = new Movable(10, 50) // 初期座標の指定
+// ボールをx方向に5、y方向に3動かす
+ball.move(5, 3);
+// 座標をブラウザのコンソールに出力
+console.log(ball.pos.x);
+console.log(ball.pos.y);
