e5fac6a59a072c9d7e970ab4176ce9c376ce799a
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 11:02:06 +0900

    進捗p72

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -1,12 +1,21 @@
 `use strict`
 
-Vue.filter('number_format', function (val) {
-    return val.toLocaleString();
-});
+// Vue.filter('number_format', function (val) {
+//     return val.toLocaleString();
+// });
 
 var app = new Vue({
     el: '#app',
     data: {
         price: 1000
+    },
+    filters: {
+        number_format: function (val) {
+            return val.toLocaleString();
+        },
+        // priceの後ろに単位をつける
+        unit: function (val) {
+            return val + '円';
+        }
     }
 });
