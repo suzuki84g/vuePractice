c10ff5f5f4fdb3ec38b026186c617fa33775cf96
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 12:37:12 +0900

    p92 一方通行のバインディング

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -3,14 +3,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        //初期値は当年
-        year: (new Date()).getFullYear()
-    },
-    methods: {
-        // 「年」のinputイベントハンドラ
-        yearInputHandler: function ($event) {
-            // 直接データを更新する
-            this.year = $event.target.value;
-        }
+        message: 'これは一方通行のバインドです。'
     }
 });
