b4cc580b846470c7717d86f328b7d116e24258c2
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 11:20:42 +0900

    p76完了

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -7,15 +7,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-        price: 1000
+        year: (new Date()).getFullYear()
     },
-    filters: {
-        number_format: function (val) {
-            return val.toLocaleString();
-        },
-        // priceの後ろに単位をつける
-        unit: function (val) {
-            return val + '円';
+    computed: {
+        //今年がうるう年か判断するproperty
+        isUrudoshi: function () {
+            // 「4で割り切れて100で割り切れない」または「400で割り切れる」場合
+            if ((this.year % 4 == 0) && (this.year % 100 != 0) ||
+                (this.year % 400 == 0)) {
+                //うるう年
+                return true;
+            } else {
+                //うるう年でない
+                return false;
+            }
         }
     }
 });
