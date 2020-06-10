c9f578c52aa2f56aecce5df42420bdcc51855d8f
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 11:27:14 +0900

    進捗p79 イベントハンドリング

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -7,20 +7,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        year: (new Date()).getFullYear()
+        stock: 10
     },
-    computed: {
-        //今年がうるう年か判断するproperty
-        isUrudoshi: function () {
-            // 「4で割り切れて100で割り切れない」または「400で割り切れる」場合
-            if ((this.year % 4 == 0) && (this.year % 100 != 0) ||
-                (this.year % 400 == 0)) {
-                //うるう年
-                return true;
-            } else {
-                //うるう年でない
-                return false;
-            }
+    methods: {
+        // 削除ボタンのクリックイベントハンドラ
+        onDeleteItem: function () {
+            this.stock--;
         }
     }
 });
