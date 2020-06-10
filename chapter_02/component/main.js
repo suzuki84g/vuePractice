ba5402a9e94eed80d08f517e2a9135a0988c806e
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 15:08:48 +0900

    p104 rgb表記

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -3,27 +3,32 @@
 var app = new Vue({
     el: '#app',
     data: {
-        arrival_date: null,
-        min_date: null
+        color: '#000000',
+        red: 0,
+        blue: 0,
+        green: 0
     },
-    created: function () {
-        /* // 初期値を設定する
-        this.arrival_date = this.formatDate(new Date()); */
-        // 翌日の日付を初期値とする
-        var dt = new Date();
-        dt.setDate(dt.getDate() + 1);
-        this.arrival_date = this.formatDate(dt);
-        // 翌日の日付を最小値とする
-        this.min_date = this.arrival_date;
+    computed: {
+        // 赤・緑・青を配列で返す算出プロパティ
+        colorElement: function () {
+            return [this.red, this.green, this.blue];
+        }
     },
-    methods: {
-        // 日付を整形
-        formatDate: function (dt) {
-            var y = dt.getFullYear();
-            var m = ('00' + (dt.getMonth() + 1)).slice(-2);
-            var d = ('00' + dt.getDate()).slice(-2);
-            var result = y + '-' + m + '-' + d;
-            return result;
+    watch: {
+        // 3色の変更を監視する
+        colorElement: function (newRGB, oldRGB) {
+            // 三色を2桁の16進数表記に変換する
+            var r = ('00' + newRGB[0].toString(16).toUpperCase()).slice(-2);
+            var g = ('00' + newRGB[1].toString(16).toUpperCase()).slice(-2);
+            var b = ('00' + newRGB[2].toString(16).toUpperCase()).slice(-2);
+            // #RRGGBB 形式の文字列に変換
+            this.color = '#' + r + g + b;
+        },
+        // カラーパレットの選択変更を監視する
+        color: function (newColor, oldColor) {
+            this.red = parseInt(newColor.substr(1, 2), 16);  // substr(文字列の位置, 何文字か)
+            this.green = parseInt(newColor.substr(3, 2), 16);
+            this.blue = parseInt(newColor.substr(5, 2), 16);
         }
     }
 });
