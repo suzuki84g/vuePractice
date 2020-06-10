7ccdb162250d5f77beaa4400854d578880a65ee2
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 11:47:30 +0900

    p82 イベントハンドリングでWindowのサイズを変更

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -7,12 +7,24 @@
 var app = new Vue({
     el: '#app',
     data: {
-        stock: 10
+        // ウィンドウサイズ
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    created: function () {
+        // イベントハンドラを登録
+        addEventListener('resize', this.resizeHandler);
+    },
+    beforeDestroy: function () {
+        // イベントハンドラを解除
+        removeEventListener('resize', this.resizeHandler);
     },
     methods: {
-        // 削除ボタンのクリックイベントハンドラ
-        onDeleteItem: function () {
-            this.stock--;
+        // イベントハンドラ
+        resizeHandler: function ($event) {
+            // 現在のウィンドウサイズでpropertyを更新
+            this.width = $event.target.innerWidth;
+            this.height = $event.target.innerHeight;
         }
     }
 });
