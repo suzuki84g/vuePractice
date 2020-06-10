3bddf68e34c5b7ce008c3411728c987497710ee3
suzuki84g <suzuki.84g@gmail.com>
2020-06-10 12:32:19 +0900

    p91 リアルタイム変更

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -5,5 +5,12 @@ var app = new Vue({
     data: {
         //初期値は当年
         year: (new Date()).getFullYear()
+    },
+    methods: {
+        // 「年」のinputイベントハンドラ
+        yearInputHandler: function ($event) {
+            // 直接データを更新する
+            this.year = $event.target.value;
+        }
     }
 });
