e603a9c26f5e42bbc70172ee809bcb23a91ec5a7
suzuki84g <suzuki.84g@gmail.com>
2020-06-11 14:46:52 +0900

    p109 2-10 フェードイン・アウト

--- chapter_02/component/main.js
+++ chapter_02/component/main.js
@@ -3,32 +3,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        color: '#000000',
-        red: 0,
-        blue: 0,
-        green: 0
-    },
-    computed: {
-        // 赤・緑・青を配列で返す算出プロパティ
-        colorElement: function () {
-            return [this.red, this.green, this.blue];
-        }
-    },
-    watch: {
-        // 3色の変更を監視する
-        colorElement: function (newRGB, oldRGB) {
-            // 三色を2桁の16進数表記に変換する
-            var r = ('00' + newRGB[0].toString(16).toUpperCase()).slice(-2);
-            var g = ('00' + newRGB[1].toString(16).toUpperCase()).slice(-2);
-            var b = ('00' + newRGB[2].toString(16).toUpperCase()).slice(-2);
-            // #RRGGBB 形式の文字列に変換
-            this.color = '#' + r + g + b;
-        },
-        // カラーパレットの選択変更を監視する
-        color: function (newColor, oldColor) {
-            this.red = parseInt(newColor.substr(1, 2), 16);  // substr(文字列の位置, 何文字か)
-            this.green = parseInt(newColor.substr(3, 2), 16);
-            this.blue = parseInt(newColor.substr(5, 2), 16);
-        }
+        show: true
     }
 });
