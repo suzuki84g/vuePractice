2ad8f18b1a4b263b85afb2aac15253224671dbb1
suzuki84g <suzuki.84g@gmail.com>
2020-06-15 12:11:47 +0900

    data値のサンプル

--- chapter_03/main.js
+++ chapter_03/main.js
@@ -3,6 +3,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        // 表示中の商品数
+        count: 0,
+        // セール状態絞り込みチェック
+        showSaleItem: false,
+        // 送料無料絞り込みチェック
+        showDelvFree: false,
+        // 並び替えフィルタリング（1：標準、2：安い順）
+        sortOrder: 1,
+        /* 商品情報
+        商品名
+        商品画像
+        価格
+        セール対象
+        送料
+        */
+        products: [
+            {
+                name: hoge,
+                price: 1234,
+                image: hoge,
+                delv: 1234,
+                isSale: false
+            }
+        ]
     }
-})
+});
