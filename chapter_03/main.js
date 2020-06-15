82385b09c3ed671eec5c94ccc278623f69d31aaa
suzuki84g <suzuki.84g@gmail.com>
2020-06-15 12:22:37 +0900

    p138 3-3完了

--- chapter_03/main.js
+++ chapter_03/main.js
@@ -19,13 +19,12 @@ var app = new Vue({
         送料
         */
         products: [
-            {
-                name: hoge,
-                price: 1234,
-                image: hoge,
-                delv: 1234,
-                isSale: false
-            }
+            { name: 'Michel<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true },
+            { name: 'Raphael<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true },
+            { name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true },
+            { name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true },
+            { name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false },
+            { name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false },
         ]
     }
 });
