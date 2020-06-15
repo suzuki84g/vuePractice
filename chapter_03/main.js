2c019edf99d0c5d358d080dd3ad8fe39e92e727d
suzuki84g <suzuki.84g@gmail.com>
2020-06-15 15:41:38 +0900

    バグがある

--- chapter_03/main.js
+++ chapter_03/main.js
@@ -43,5 +43,31 @@ var app = new Vue({
             // ここでproductの配列を書き換える
             console.log("showDelvFreeウォッチャが呼び出されたよ")
         }
+    },
+    computed: {
+        // 絞り込み後の商品リストを返す算出プロパティ
+        filteredList: function () {
+            // 絞り込み後の商品リストを格納する新しい配列
+            var newList = [];
+            for (var i = 0; i < this.products.length; i++) {
+                // 表示対象かどうかを判定するフラグ
+                var isShow = true;
+                // i番目の商品が表示対象日どうかを判定する
+                if (this.showSaleItem && !this.products[i].isSale) {
+                    // 「セール対象」チェック有りで、セール対象商品ではない場合
+                    isShow = false;  // この商品は表示しない
+                }
+                if (this.showDelvFree && this.products[i].delv > 0) {
+                    // 「送料無料」チェック有りで、送料ありの商品の場合
+                    isShow = false;  // この商品は表示しない
+                    // 表示対象の商品だけを新しい配列に追加する
+                    if (isShow) {
+                        newList.push(this.products[i]);
+                    }
+                }
+                // 絞り込み後の商品リストを返す
+                return newList;
+            }
+        }
     }
 });
