f1dc82dcdb5fbaa0352e09f2f2c99ed3a8a61687
suzuki84g <suzuki.84g@gmail.com>
2020-06-16 11:48:26 +0900

    p166 進捗

--- chapter_04/4-2 test/main.js
+++ chapter_04/4-2 test/main.js
@@ -5,4 +5,20 @@ var btnLoad = document.querySelector('#load');
 btnLoad.addEventListener('click', clickHandler);
 function clickHandler(event) {
     // TODO:JSON読み込み時の処理を記述
-}
+    // 1. XMLHttpRequestオブジェクトのインスタンスを生成
+    var xmlHttpRequest = new XMLHttpRequest();
+    // 2. 通信状態の変化を関しするイベントハンドラの設定
+    xmlHttpRequest.onreadystatechange = function () {
+        // 受信が正常に完了した場合をtrue
+        if (this.readyState == 4 && this.status == 200) {
+            // 受信データをコンソール出力
+            console.log(this.readyState, this.response);
+        }
+    };
+    // 3. レスポンス形式の指定
+    xmlHttpRequest.responseType = 'JSON';
+    // 4. requestメソッドと読み込むファイルのパスを指定
+    xmlHttpRequest.open('GET', 'products.json');
+    // 5. requestの送信（非同期通信の開始）
+    xmlHttpRequest.send();
+};
