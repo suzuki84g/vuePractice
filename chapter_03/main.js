6325c681d6b87812cffe364cf1f8e6b22bfe582b
suzuki84g <suzuki.84g@gmail.com>
2020-06-16 11:01:17 +0900

    p154 idを付与

--- chapter_03/main.js
+++ chapter_03/main.js
@@ -24,12 +24,12 @@ var app = new Vue({
         送料
         */
         products: [
-            { name: 'Michel<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true },
-            { name: 'Raphaelf<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true },
-            { name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true },
-            { name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true },
-            { name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false },
-            { name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false },
+            { id: 1, name: 'Michel<br>スマホケース', price: 1580, image: 'images/01.jpg', delv: 0, isSale: true },
+            { id: 2, name: 'Raphaelf<br>スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true },
+            { id: 3, name: 'Gabriel<br>スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true },
+            { id: 4, name: 'Uriel<br>スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true },
+            { id: 5, name: 'Ariel<br>スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false },
+            { id: 6, name: 'Azrael<br>スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false },
         ]
     },
     watch: {
