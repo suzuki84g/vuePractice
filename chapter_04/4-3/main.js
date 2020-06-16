fe288db7ed0682c9080ed2c34cf5d02917d4bf64
suzuki84g <suzuki.84g@gmail.com>
2020-06-16 14:42:26 +0900

    p179 4-3まで完了

--- chapter_04/4-3/main.js
+++ chapter_04/4-3/main.js
@@ -6,7 +6,6 @@
 // 読み込みボタンのクリックイベントハンドラを定義
 $('#load').on('click', clickHandler);
 function clickHandler(event) {
-    // TODO:ここにJSONを読み込む処理を記述
     // 非同期通信によるJSON読み込み
     // `$.ajax({オブジェクト表記パラメータ}).done(コールバック関数).fail(コールバック関数)`ここまで繋がっている
     // イベントハンドラ＝＝予め仕込んでおいたら勝手に呼び出される関数＝＝コールバック関数
@@ -17,12 +16,30 @@ function clickHandler(event) {
         dataType: 'json'        // responseのデータタイプ
     })
         .done(function (data, textStatus, jqXHR) {
-            // ここに通信成功時の処理を記述
-            console.log('通信は成功です。');
-            console.log(jqXHR);  // プロミス表示
+            // 通信成功時の処理
+            updateScreen(data)  // datatypeで指定したものが渡される。ここではjson、全量はjqXHR。
         })
         .fail(function (jqXHR, textStatus, errorThrown) {
             // ここに通信失敗時の処理を記述
             console.log('通信は失敗しました。');
         });
+}
+
+// 商品一覧の描画を更新する
+function updateScreen(products) {
+    // 商品リストの子ノード全削除
+    $('#result').empty();
+    // 商品の子ノードをDOMに挿入
+    var list = '';  // 空のlistを宣言
+    for (var i = 0; i < products.length; i++) {
+        list += '<div>';
+        list += '商品ID:' + products[i].id;
+        list += ' 商品名:' + products[i].name;
+        list += ' 料金:' + products[i].price;
+        list += ' 画像パス:' + products[i].image;
+        list += ' 送料:' + products[i].delv;
+        list += ' セール対象:' + products[i].isSale;
+        list += '</div>';
+    }
+    $('#result').append(list);
 }
