682db6e459d569f62f570ef5fd21c7b314faf00d
suzuki84g <suzuki.84g@gmail.com>
2020-06-16 13:49:41 +0900

    p172 期待値通りの動作を確認

--- chapter_04/4-2_test/main.js
+++ chapter_04/4-2_test/main.js
@@ -4,15 +4,29 @@ var btnLoad = document.querySelector('#load');
 // 読み込みボタンのクリックイベントハンドラを定義
 btnLoad.addEventListener('click', clickHandler);
 function clickHandler(event) {
-    // TODO:JSON読み込み時の処理を記述
     // 1. XMLHttpRequestオブジェクトのインスタンスを生成
     var xmlHttpRequest = new XMLHttpRequest();
     // 2. 通信状態の変化を関しするイベントハンドラの設定
     xmlHttpRequest.onreadystatechange = function () {
         // 受信が正常に完了した場合をtrue（ローカルのfileプロトコル使用時はステータス確認をオフ）
-        if (this.readyState /*== 4 && this.status == 200*/) {
-            // 受信データをコンソール出力
-            console.log(this.readyState, this.response);
+        if (this.readyState == 4 /*&& this.status == 200*/) {
+            // jsonを変数に格納
+            var products = this.response;
+            // 商品リストの子ノードを全て削除する
+            var result = document.querySelector('#result')
+            result.textContent = '';
+            // 商品の子ノードをDOMに挿入する
+            for (var i = 0; i < products.length; i++) {
+                var text = '商品ID:' + products[i].id;
+                text += ' 商品名:' + products[i].name;
+                text += ' 料金:' + products[i].price;
+                text += ' 画像パス:' + products[i].image;
+                text += ' 送料:' + products[i].delv;
+                text += ' セール対象:' + products[i].isSale;
+                var div = document.createElement('div');
+                div.textContent = text;
+                result.appendChild(div);
+            }
         }
     };
     // 3. レスポンス形式の指定
