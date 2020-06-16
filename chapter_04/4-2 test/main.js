40078d389624ac682e0419d8635ed4486a507f4a
suzuki84g <suzuki.84g@gmail.com>
2020-06-16 11:39:13 +0900

    p165

---
+++ chapter_04/4-2 test/main.js
@@ -0,0 +1,8 @@
+`use strict`
+
+var btnLoad = document.querySelector('#load');
+// 読み込みボタンのクリックイベントハンドラを定義
+btnLoad.addEventListener('click', clickHandler);
+function clickHandler(event) {
+    // TODO:JSON読み込み時の処理を記述
+}
