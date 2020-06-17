c1f1551bc9d8c21532f67ed11894bed0ec3f82fb
suzuki84g <suzuki.84g@gmail.com>
2020-06-17 11:23:03 +0900

    p183完了 ResponseHeaderのcontentsTypeが肝要

--- chapter_04/4-4/main.js
+++ chapter_04/4-4/main.js
@@ -2,11 +2,11 @@
 
 // JSONPのURL（githubのrepositoryに上げたファイル）
 var url = 'https://raw.githubusercontent.com/suzuki84g/vuePractice/dev/chapter_04/4-4/jsonp.js';
-var urlLocal ='jsonp.js'
+var urlLocal ='http://localhost:3030/jsonp.js'
 
 // 非同期通信でJSONPの読み込み
 $.ajax({
-    url: url,
+    url: urlLocal,
     type: 'GET',
     dataType: 'jsonp',          // データタイプ
     jsonp: 'callback',          // クエリパラメータの名前
