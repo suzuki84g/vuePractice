bad751dd20c117a25264dfbceced8b093a182e9e
suzuki84g <suzuki.84g@gmail.com>
2020-06-23 11:51:04 +0900

    p243 進捗

--- chapter_06/show-hallo.js
+++ chapter_06/show-hallo.js
@@ -1,3 +1,27 @@
 `use strict`
 
-Vue.component('show-hello', { template: '<p>hello Vue!!</p>' });
+Vue.component('show-hello', {
+    // テンプレート
+    template: '<p>{{message}}</p>',
+    // data
+    data: function () {
+        return {
+            message: 'Hello.'
+        }
+    },
+    // メソッド
+    methods: {
+    },
+    // 算出プロパティ
+    computed: {
+    },
+    // ウォッチャ
+    watch: {
+    },
+    // フィルター
+    filter: {
+    },
+    // ライフサイクルハック
+    created: function () {
+    }
+});
