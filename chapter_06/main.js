c09d9113950cf7a49185a47dc8e0e0a7b86edccc
suzuki84g <suzuki.84g@gmail.com>
2020-06-23 13:27:16 +0900

    p257 forとコンポーネント

--- chapter_06/main.js
+++ chapter_06/main.js
@@ -1,24 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name: 'スマホケース',
-        price: 880
-    },
-    methods: {
-        priceDown: function () {
-            var discount = 0;
-            if (this.price - 50 < 500) {
-                // 例）現在の価格が530円の場合、値下げ幅は30円
-                discount = this.price - 500;
-            } else {
-                // 例）現在の価格が550円以上の場合、値下げ幅は50円
-                discount = 50;
-            }
-            // // 第荷引数が追加されている
-            // this.$emit('child-click', discount);
-
-            // 値引きする
-            this.price -= discount;
-        }
+        // 商品データ
+        products: [
+            { id: 1, name: "商品A", price: 1280 },
+            { id: 2, name: "商品B", price: 1580 },
+            { id: 3, name: "商品C", price: 1980 },
+        ]
     }
 });
