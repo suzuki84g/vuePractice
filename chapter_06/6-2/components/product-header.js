2e50c64ba8ae4a69d112bb50139da721414320b4
suzuki84g <suzuki.84g@gmail.com>
2020-06-26 23:20:48 +0900

    p271 jsを記述

--- chapter_06/6-2/components/product-header.js
+++ chapter_06/6-2/components/product-header.js
@@ -1,1 +1,28 @@
 `use script`
+
+var productHeader = {
+    template: `
+    <header>
+        <h1 class="pageTitle">商品一覧</h1>
+        <!-- 検索欄 -->
+        <div class="search">
+            <div class="result">
+                検索結果<span class="count">{{count}}件</span>
+            </div>
+            <div class="condition">
+                <div class="target">
+                    <label><input type="checkbox" v-bind:checked="showSaleItem" v-on:change="$emit('showSaleItemChanged')">セール対象{{showSaleItem}}</label>
+                    <label><input type="checkbox" v-bind:checked="showDelvFree" v-on:change="$emit('showDelvFreeChanged')">送料無料{{showDelvFree}}</label>
+                </div>
+                <div class="sort">
+                    <label for="sort">並び替え</label>
+                    <select id="sort" class="sorting" v-bind:value="sortOrder" v-change="$emit('sortOrderChanged', parseInt($event.target.value))">
+                        <option value="1">標準</option>
+                        <option value="2">価格が安い順</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </header>`,
+    props: ['count', 'showSaleItem', 'showDelvFree', 'sortOrder']
+};
