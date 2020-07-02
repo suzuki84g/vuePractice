646a6140ff654e3fad1caf1b314fa222c53888ee
suzuki84g <suzuki.84g@gmail.com>
2020-07-02 13:34:16 +0900

    記述

---
+++ chapter_06/6-5/my-product02/src/components/product-list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+      <product-header
+          v-bind:count="filteredList.length"
+          v-bind:showSaleItem="showSaleItem"
+          v-bind:showDelvFree="showDelvFree"
+          v-bind:sortOrder="sortOrder"
+          v-on:showSaleItemChanged="showSaleItem=!showSaleItem"
+          v-on:showDelvFreeChanged="showDelvFree=!showDelvFree"
+          v-on:sortOrderChanged="sortOrderChanged"
+      >
+      </product-header>
+      <div class="list">
+          <product
+              v-for="product in filteredList"
+              v-bind:product="product"
+              v-bind:key="product.id">
+          </product>
+      </div>
+  </div>
+</template>
+
+<script>
+import productHeader from './product-header.vue';
+import product from './product.vue'
+
+export default {
+  
+}
+</script>
